694327e29b5074fc7b0069a77ff66d9d0ec5627e
aahi-arsyam <115212828+aahi-arsyam@users.noreply.github.com>
2022-10-08 14:13:14 +0530

    Create max path sum in two arrays.cpp

---
+++ max path sum in two arrays.cpp
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    /*You are required to complete this method*/
+     int max_path_sum(int A[], int B[], int l1, int l2)
+    {
+    
+        //Your code here
+        int i=0, j=0;
+        int sumA = 0, sumB=0, maxSum = 0;
+        while(i<l1 && j<l2)
+        {
+            if(A[i] < B[j])
+                sumA += A[i++];
+            else if(A[i] > B[j])
+                sumB += B[j++];
+            else
+            {
+                maxSum += max(sumA, sumB);
+                maxSum += A[i];
+                i++; j++;
+                sumA = 0;
+                sumB = 0;
+            }
+        }
+        while(i<l1)
+            sumA += A[i++];
+        
+        while(j<l2)
+            sumB += B[j++];
+        
+        maxSum += max(sumA, sumB);
+        return maxSum;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int T;
+    cin>>T;
+
+    while(T--)
+    {
+        int N,M;
+        cin>>N>>M;
+        fflush(stdin);
+        int a[N],b[M];
+        for(int i=0;i<N;i++)
+            cin>>a[i];
+        for(int i=0;i<M;i++)
+            cin>>b[i];
+        Solution obj;
+        int result = obj.max_path_sum(a,b,N,M);
+        cout<<result<<endl;
+    }
+}
+
+
+// } Driver Code Ends
