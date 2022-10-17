df067b5d9ef258fa1f5c742ef4296f8e15983793
aahi-arsyam <115212828+aahi-arsyam@users.noreply.github.com>
2022-10-17 19:41:05 +0530

    Create Product array puzzle.cpp

---
+++ Product array puzzle.cpp
@@ -0,0 +1,81 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+  public:
+    // nums: given vector
+    // return the Product vector P that hold product except self at each index
+    vector<long long int> productExceptSelf(vector<long long int>& nums, int n) {
+       
+        //code here      
+        vector<long long int> p;
+        
+        long long int prod = 1;
+        long long left[n], right[n];
+        
+        if(n == 1)
+        {
+            p.push_back(1);
+            return p;
+        }
+        
+        left[0] = nums[0];
+        for(int i=1; i<n; i++)
+        {
+            left[i] = left[i-1]*nums[i];
+        }
+        
+        right[n-1] = nums[n-1];
+        for(int i=n-2; i>=0; i--)
+        {
+            right[i] = right[i+1]*nums[i];
+        }
+        
+        p.push_back(right[1]);
+        for(int i=1; i<n-1; i++)
+        {
+            long long temp = left[i-1] * right[i+1];
+            p.push_back(temp);
+        }
+        p.push_back(left[n-2]);
+        
+        return p;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main()
+ {
+    int t;  // number of test cases
+    cin>>t;
+    while(t--)
+    {
+        int n;  // size of the array
+        cin>>n;
+        vector<long long int> arr(n),vec(n);
+        
+        for(int i=0;i<n;i++)    // input the array
+        {
+            cin>>arr[i];
+        }
+        Solution obj;
+        vec = obj.productExceptSelf(arr,n);   // function call
+        
+        for(int i=0;i<n;i++)    // print the output
+        {
+            cout << vec[i] << " ";
+        }
+        cout<<endl;
+    }
+	return 0;
+}
+// } Driver Code Ends
