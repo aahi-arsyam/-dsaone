d515a3fa2db8906fbe505f9cbebf2152e28340d3
aahi-arsyam <115212828+aahi-arsyam@users.noreply.github.com>
2022-10-19 16:51:26 +0530

    Update Product array puzzle.cpp

--- Product array puzzle.cpp
+++ Product array puzzle.cpp
@@ -15,7 +15,8 @@ class Solution{
     // return the Product vector P that hold product except self at each index
     vector<long long int> productExceptSelf(vector<long long int>& nums, int n) {
        
-        //code here      
+        //code here   
+	// O(n) time and O(n) space solution
         vector<long long int> p;
         
         long long int prod = 1;
@@ -48,6 +49,37 @@ class Solution{
         p.push_back(left[n-2]);
         
         return p;
+	    
+	/* O(n) time and O(1) space solution
+	vector<long long int> p;
+        
+        long long int prod = 1;
+        long long count = 0;
+        
+        for(int i=0; i<n; i++)
+        {
+            if(nums[i] == 0)
+                count++;
+            else 
+                prod *= nums[i];
+        }
+        
+        for(int i=0; i<n; i++)
+        {
+            if(count > 1)
+                p.push_back(0);
+                
+            if(count == 0)
+                p.push_back(prod/nums[i]);
+            
+            else if(count == 1 && nums[i] == 0)
+                p.push_back(prod);
+            
+            else 
+                p.push_back(0);
+        }
+        
+        return p;*/
     }
 };
 
